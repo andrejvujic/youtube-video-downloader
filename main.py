a6ab73093ae1f9a7a1eea433d20f5786af8bc8ee
Andrej Vujic <vujicandrej365@gmail.com>
2021-03-13 15:54:49 +0100

    removed firebase

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 from pytube import YouTube
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import storage
 import uuid
 
 video_url = "https://www.youtube.com/watch?v=xWOoBJUqlbI"
@@ -16,22 +13,15 @@ def download_video(video_url=""):
     FILE_NAME = get_uuid()
     try:
         yt = YouTube(video_url)
+        print("video initialized")
     except:
+        print("error while initializing")
         return
 
+    print("video downloading")
     yt.streams.filter(progressive=True, file_extension='mp4').order_by(
         'resolution').desc().first().download(output_path=OUTPUT_PATH, filename=FILE_NAME)
-
-    upload_video(file_name=FILE_NAME)
-
-
-def upload_video(file_name):
-    STORAGE_BUCKET = 'yt-dwld.appspot.com'
-
-    credentials = credentials.Certificate('service-account.json')
-    firebase_admin.initialize_app(credentials, {
-        'storageBucket': STORAGE_BUCKET,
-    })
+    print("video downloaded")
 
 
 download_video(video_url=video_url)
