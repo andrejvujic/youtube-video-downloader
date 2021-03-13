cdc2743e29a6336c5a873fba3b378b5accca5a4e
Andrej Vujic <vujicandrej365@gmail.com>
2021-03-13 16:58:55 +0100

    added deleting downloaded file from server

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from pytube import YouTube
 from flask import Flask, request, send_file
 import uuid
+import io
+import os
 
 
 def get_uuid():
@@ -31,4 +33,13 @@ def download_video():
     yt.streams.filter(progressive=True, file_extension='mp4').order_by(
         'resolution').desc().first().download(output_path="downloads/", filename=FILE_NAME)
     print("video downloaded")
-    return send_file(f"downloads/{FILE_NAME}.mp4", as_attachment=True)
+
+    return_data = io.BytesIO()
+    with open(f"downloads/{FILE_NAME}.mp4", 'rb') as fo:
+        return_data.write(fo.read())
+    return_data.seek(0)
+
+    os.remove(f"downloads/{FILE_NAME}.mp4")
+
+    return send_file(return_data,
+                     attachment_filename=f"downloads/{FILE_NAME}.mp4")
